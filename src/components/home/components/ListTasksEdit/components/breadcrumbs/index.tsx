ac9dcd8e3ec874c73a1576ff04ddd7620e101e67
robsonnsbr <robsonnsbr@gmail.com>
2024-08-18 21:02:04 -0300

    feat: include QTfiltered

--- src/components/home/components/ListTasksEdit/components/breadcrumbs/index.tsx
+++ src/components/home/components/ListTasksEdit/components/breadcrumbs/index.tsx
@@ -3,12 +3,22 @@ import { useTasksContext } from 'src/contexts/tasksProvider';
 import { useFilterPhrase } from 'src/hooks';
 
 const BreadcrumbsFilter = () => {
-  const { filters } = useTasksContext();
-  const phrase = useFilterPhrase(filters);
+  const { filters, filteredTasks } = useTasksContext();
+  const { phrase, qtFiltered } = useFilterPhrase(filters, filteredTasks);
 
   return (
-    <nav title="filter">
-      <span className="font-inter text-xs text-deepGray">{phrase}</span>
+    <nav
+      title="filter"
+      className="flex justify-between font-inter text-xs text-deepGray font-medium"
+    >
+      <span>{phrase}</span>
+      <span>
+        {qtFiltered > 0
+          ? qtFiltered === 1
+            ? `${qtFiltered} Nota`
+            : `${qtFiltered} Notas`
+          : 'Nenhuma'}
+      </span>
     </nav>
   );
 };
